79a30c8d40278beb6bd553b1507397d9cf50882b
fjb2020 <92430217+fjb2020@users.noreply.github.com>
2026-01-01 10:27:36 +0000

    UI changes to accommodate changes to metadata and keyword handling

--- piwigoPublish.lrplugin/PublishDialogSections.lua
+++ piwigoPublish.lrplugin/PublishDialogSections.lua
@@ -288,42 +288,38 @@ local function prefsDialog (f, propertyTable)
 
 		f:spacer { height = 2 },
 		f:separator { fill_horizontal = 1},
+		f:group{
+			title = "Keyword Options and Settings",
+			font = "<system/bold>",
 
-		f:row {
-			f:static_text {
-				title = "Settings for Transferring Metadata to Piwigo",
-				font = "<system/bold>",
-				alignment = 'left',
-				fill_horizontal = 1,
-			},
-		},
-		f:spacer { height = 2 },
-		f:row {
-			f:static_text {
-				title = "",
-				alignment = 'left',
-				width_in_chars = 7,
-			},
-			f:checkbox {
-				title = "Include Full Keyword Hierarchy",
-				tooltip = "If checked, all keywords in a keyword hierarchy will be sent to Piwigo",
-				value = bind 'KwFullHierarchy',
-			}
+			f:spacer { height = 2 },
+			f:row {
+				f:static_text {
+					title = "",
+					alignment = 'left',
+					width_in_chars = 7,
+				},
+				f:checkbox {
+					title = "Include Full Keyword Hierarchy",
+					tooltip = "If checked, all keywords in a keyword hierarchy will be sent to Piwigo",
+					value = bind 'KwFullHierarchy',
+				}
 
-		},
-		f:spacer { height = 2 },
-		f:row {
-			f:static_text {
-				title = "",
-				alignment = 'left',
-				width_in_chars = 7,
 			},
-			f:checkbox {
-				title = "Include Keyword Synonyms",
-				tooltip = "If checked, keyword synonyms will be sent to Piwigo",
-				value = bind 'KwSynonyms',
-			}
+			f:spacer { height = 2 },
+			f:row {
+				f:static_text {
+					title = "",
+					alignment = 'left',
+					width_in_chars = 7,
+				},
+				f:checkbox {
+					title = "Include Keyword Synonyms",
+					tooltip = "If checked, keyword synonyms will be sent to Piwigo",
+					value = bind 'KwSynonyms',
+				}
 
+			},
 		},
 	}
 end
